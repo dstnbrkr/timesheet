3f5df4b3a135ebef935673775d249138c27dbf24
dbarker <dustin.barker@agilepartners.com>
2008-11-08 15:51:41 -0500

    add test for invalid command

--- test/test_application.rb
+++ test/test_application.rb
@@ -12,12 +12,25 @@ class TestApplication < Test::Unit::TestCase
     end
   end
 
-  def test_no_command
+  def test_display_usage_when_no_command
+    set_argv []
+    assert_match(/usage/i, capture_system_error)
+  end
+
+  def test_display_usage_when_invalid_command
+    set_argv "foo"
+    assert_match(/unknown/i, capture_system_error)
+  end
+
+  def set_argv(argv)
     ARGV.clear
-    err = capture_stderr { 
+    argv.each { |e| ARGV << e }
+  end
+
+  def capture_system_error(&block)
+    capture_stderr { 
       catch(:system_exit) { @app.run }
     }
-    assert_match(/usage/i, err)
-  end 
+  end
 
 end
